6e8237a7eae744bf1c6bde08fe26a782634d2840
turbovadim <yolkin.vadim@gmail.com>
2026-01-14 10:54:23 +0300

    support 1.21.11

---
+++ 1.21.11/build.gradle.kts
@@ -0,0 +1,39 @@
+import io.papermc.paperweight.userdev.ReobfArtifactConfiguration
+
+plugins {
+    id("java")
+    id("io.papermc.paperweight.userdev")
+    kotlin("jvm") version "2.2.21"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+
+    maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/") {
+        name = "sonatype-oss-snapshots"
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(project(":version"))
+    paperweight.paperDevBundle("1.21.11-R0.1-SNAPSHOT")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    disableAutoTargetJvm()
+}
+
+tasks {
+    compileJava {
+        options.release.set(21)
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
